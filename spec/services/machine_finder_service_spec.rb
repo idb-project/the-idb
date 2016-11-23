949ce8ceeb66f40c471f3874ab7e3193759e74e3
Daniel Rauer <rauer@bytemine.net>
2016-11-23 13:20:27 +0100

    Improve some machine_finder specs.

--- spec/services/machine_finder_service_spec.rb
+++ spec/services/machine_finder_service_spec.rb
@@ -14,14 +14,14 @@ describe MachineFinderService do
     it 'creates a machine that has been found in a PuppetDB but not locally' do
       @mfs.instance_variable_set(:@nodes, ["test2.example.com"]) 
 
-      expect(MachineUpdateWorker).to receive(:perform_async).with("test2.example.com")
       @mfs.find_untracked_machines
+      expect(Machine.last.fqdn).to eq("test2.example.com")
     end
 
     it 'does not create a machine that already exists' do
       @mfs.instance_variable_set(:@nodes, ["test.example.com"]) 
 
-      expect(MachineUpdateWorker).not_to receive(:perform_async).with("test.example.com")
+      expect(Machine).not_to receive(:new).with(fqdn: "test.example.com")
       @mfs.find_untracked_machines
     end
 
