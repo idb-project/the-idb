a165c3406b0a523a626510364505721c4a9f1acf
Daniel Rauer <rauer@bytemine.net>
2016-11-25 13:44:05 +0100

    Adapt WarrantyReminderWorker to new InventoryStatus model. Also add specs.

---
+++ spec/worker/warranty_reminder_worker_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe WarrantyReminderWorker do
+  describe "perform" do
+    before(:each) do
+      @w = WarrantyReminderWorker.new
+    end
+
+    it "does not send out an email without any inventory items existing" do
+      expect(@w.perform).to be_nil
+    end
+
+    it "does not send out an email without inventory items that have a warranty end date" do
+      create(:inventory)
+      expect(@w.perform).to be_nil
+    end
+
+    it "does not send out an email without inventory items that have a non-matching warranty end date" do
+      create(:inventory, warranty_end: (DateTime.now+2.years).strftime("%Y-%m-%d"))
+      expect(@w.perform).to be_nil
+    end
+
+    it "does not send out an email with inventory items that have a matching warranty end date but status inactive" do
+      create(:inventory, warranty_end: (DateTime.now+2.weeks).strftime("%Y-%m-%d"), inventory_status: create(:inventory_status, inactive: true))
+      expect(@w.perform).to be_nil
+    end
+
+    it "does send out an email with inventory items that have a matching warranty end date" do
+      create(:inventory, warranty_end: (DateTime.now+2.weeks).strftime("%Y-%m-%d"), inventory_status: create(:inventory_status, inactive: false))
+      expect(@w.perform).to be_instance_of(Mail::Message)
+    end
+  end
+end
