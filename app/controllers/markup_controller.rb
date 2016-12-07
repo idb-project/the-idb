248675470dbfa5897b1ed52f5633e8184a42694d
Daniel Rauer <rauer@bytemine.net>
2016-12-07 11:48:46 +0100

    Fix deprecation warning.

--- app/controllers/markup_controller.rb
+++ app/controllers/markup_controller.rb
@@ -1,5 +1,5 @@
 class MarkupController < ApplicationController
   def do_render
-    render text: TextileRenderer.render(request.body.read)
+    render plain: TextileRenderer.render(request.body.read)
   end
 end
