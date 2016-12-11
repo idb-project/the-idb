9a17bbe713447751dad1ecea01756d87629fac00
Daniel Rauer <rauer@bytemine.net>
2016-12-11 23:57:25 +0100

    Simplify software routing. Improve UI.

--- app/controllers/softwares_controller.rb
+++ app/controllers/softwares_controller.rb
@@ -1,19 +1,18 @@
 class SoftwaresController < ApplicationController
   def index
-    @software = nil
-  end
+    @machines = nil
+    if params["q"] && !params["q"].blank?
+      @software = Array.new
+      params["q"].split.each { |s|
+        n, v = s.split('=')
+        if v
+          @software << { name: n, version: v }
+        else
+          @software << { name: n }
+        end
+      }
 
-  def search
-    @software = Array.new
-    params["q"].split.each { |s| 
-      n, v = s.split('=')
-      if v
-        @software << { name: n, version: v }
-      else
-        @software << { name: n }
-      end
-    }
-
-    @machines = Machine.includes(:owner, nics: [:ip_address]).order(:fqdn).where('JSON_CONTAINS(software, ?)', ActiveSupport::JSON.encode(@software))
+      @machines = Machine.includes(:owner, nics: [:ip_address]).order(:fqdn).where('JSON_CONTAINS(software, ?)', ActiveSupport::JSON.encode(@software))
+    end
   end
 end
