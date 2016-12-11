f284311c14d869b9e41b7350237d5ebb7278c2ef
Daniel Rauer <rauer@bytemine.net>
2016-12-11 21:59:51 +0100

    Revert "Lets try switching travis CI back to docker."
    This reverts commit 10d60fcf5557486ba81c9aab55bfc11e81682d56.

--- scripts/travis-reset-mysql-root-password.sh
+++ scripts/travis-reset-mysql-root-password.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -x
 set -e
-service mysql stop || echo "mysql not stopped"
-stop mysql-5.6 || echo "mysql-5.6 not stopped"
-mysqld_safe --skip-grant-tables &
+sudo service mysql stop || echo "mysql not stopped"
+sudo stop mysql-5.6 || echo "mysql-5.6 not stopped"
+sudo  mysqld_safe --skip-grant-tables &
 sleep 4
-mysql -e "use mysql; update user set authentication_string=PASSWORD('') where User='root'; update user set plugin='mysql_native_password';FLUSH PRIVILEGES;"
-kill -9 `sudo cat /var/lib/mysql/mysqld_safe.pid`
-kill -9 `sudo cat /var/run/mysqld/mysqld.pid`
-service mysql restart
+sudo mysql -e "use mysql; update user set authentication_string=PASSWORD('') where User='root'; update user set plugin='mysql_native_password';FLUSH PRIVILEGES;"
+sudo kill -9 `sudo cat /var/lib/mysql/mysqld_safe.pid`
+sudo kill -9 `sudo cat /var/run/mysqld/mysqld.pid`
+sudo service mysql restart
 sleep 4
