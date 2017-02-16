2deec351ae4a06c31001c64736d2011d7b32da0e
Felix Kronlage <kronlage@bytemine.net>
2017-02-16 15:03:10 +0100

    another attempt to make sure we install mysql

--- scripts/travis-mysql-5.7.sh
+++ scripts/travis-mysql-5.7.sh
@@ -1,6 +1,7 @@
+export DEBIAN_FRONTEND=noninteractive
 echo mysql-apt-config mysql-apt-config/select-server select mysql-5.7 | sudo debconf-set-selections
 wget http://dev.mysql.com/get/mysql-apt-config_0.8.1-1_all.deb
 sudo dpkg --install mysql-apt-config_0.8.1-1_all.deb
 sudo apt-get update -q
-sudo apt-get install -q --allow-unauthenticated -y mysql-server
+sudo apt-get install -q -y --allow-unauthenticated mysql-server
 sudo mysql_upgrade
