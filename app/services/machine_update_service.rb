32808111cb4c7127dcdbc54e7649654ac0cfe920
Daniel Rauer <rauer@bytemine.net>
2017-02-16 13:17:44 +0100

    Better naming of variables on puppetDB package parsing.

--- app/services/machine_update_service.rb
+++ app/services/machine_update_service.rb
@@ -77,25 +77,25 @@ class MachineUpdateService
     machine.save!
   end
 
-  def self.parse_yum_packages(x)
+  def self.parse_yum_packages(packages)
     software = Array.new
-    x.gsub(/[\[\]]/,'').split(' ').each do |s|
-      m = /(?<name>.*)-(?<version>.*-.*\..*)/.match(s)
-      if m
-        software << { name: m[:name], version: m[:version] }
+    packages.gsub(/[\[\]]/,'').split(' ').each do |package|
+      matched_package = /(?<name>.*)-(?<version>.*-.*\..*)/.match(package)
+      if matched_package
+        software << { name: matched_package[:name], version: matched_package[:version] }
       end
     end
     return software
   end
 
-  def self.parse_apt_packages(x)
+  def self.parse_apt_packages(packages)
     software = Array.new
-    x.gsub(/[\[\]]/,'').split(' ').each do |s|
-      n, v = s.split('=')
-      if v
-        software << { name: n, version: v }
+    packages.gsub(/[\[\]]/,'').split(' ').each do |package|
+      name, version = package.split('=')
+      if version
+        software << { name: name, version: version }
       else
-        software << { name: n }
+        software << { name: name }
       end
     end
     return software
