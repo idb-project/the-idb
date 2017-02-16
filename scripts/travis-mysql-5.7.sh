a72871601a08b27d2335fa881e7f2cf92c2b635a
Felix Kronlage <kronlage@bytemine.net>
2017-02-16 15:19:51 +0100

    and couple more dpkg options that we had earlier

--- scripts/travis-mysql-5.7.sh
+++ scripts/travis-mysql-5.7.sh
@@ -3,5 +3,5 @@ echo mysql-apt-config mysql-apt-config/select-server select mysql-5.7 | sudo deb
 wget http://dev.mysql.com/get/mysql-apt-config_0.8.1-1_all.deb
 sudo dpkg --install mysql-apt-config_0.8.1-1_all.deb
 sudo apt-get update -q
-sudo apt-get install -qq --allow-unauthenticated --force-yes mysql-server
+sudo apt-get install -qq --allow-unauthenticated --force-yes -o Dpkg::Options::=--force-confnew -o Dpkg::Options::=--force-bad-verify mysql-server
 sudo mysql_upgrade
