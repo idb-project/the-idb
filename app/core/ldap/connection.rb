027aa24acc9ddf73fe551a6ae17c109edd38d028
Daniel Rauer <rauer@bytemine.net>
2017-03-07 23:28:18 +0100

    #4841: Improve LDAP connection code.

--- app/core/ldap/connection.rb
+++ app/core/ldap/connection.rb
@@ -7,7 +7,6 @@ module LDAP
 
     def initialize(config)
       @config = config
-      @uid = config.uid
 
       options = {
         base: config.base,
@@ -24,14 +23,14 @@ module LDAP
       end
 
       # Enable encryption if ldaps port is used.
-      options[:encryption] = :simple_tls if options[:port] == 636
+      options[:encryption] = :simple_tls if options[:port] == (636 || 7636)
       options[:encryption] = :simple_tls if config.tls == "yes"
 
       @ldap = Net::LDAP.new(options)
     end
 
     def find_user(login, password)
-      filter = Net::LDAP::Filter.eq(@uid, login)
+      filter = Net::LDAP::Filter.eq(@config.uid, login)
       response = @ldap.bind_as(:filter => filter, :password => password)
 
       response ? LDAP::User.new(response.first) : false
