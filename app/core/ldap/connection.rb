4b79f4ab0ee01f5be7ab7227307a21f7f2119056
Daniel Rauer <rauer@bytemine.net>
2017-03-07 16:34:48 +0100

    #4841: Implement LDAPConnection is_admin? method.

--- app/core/ldap/connection.rb
+++ app/core/ldap/connection.rb
@@ -3,7 +3,10 @@ require 'ldap/user'
 
 module LDAP
   class Connection
+    @config
+
     def initialize(config)
+      @config = config
       @uid = config.uid
 
       options = {
@@ -32,6 +35,23 @@ module LDAP
       response = @ldap.bind_as(:filter => filter, :password => password)
 
       response ? LDAP::User.new(response.first) : false
+    end
+
+    def is_admin?(dn)
+      if @config.admin_group.blank?
+        return true
+      else
+        if @ldap.bind
+          filter = Net::LDAP::Filter.eq(@config.group_membership_attribute, dn)
+          result = @ldap.search(base: @config.base, filter: filter)
+          result.each do |entry|
+            return true if entry.dn == @config.admin_group
+          end
+        else
+          puts "error on LDAP bind: " + @ldap.get_operation_result
+        end
+      end
+      false
     end
   end
 end
