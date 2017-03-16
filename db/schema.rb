e9e9fd032f5805b7dc6e5c746ec0e7d4de6abdea
Daniel Rauer <rauer@bytemine.net>
2017-03-16 10:56:26 +0100

    Implement migration to add ApiToken name on top of the API data hash.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170310105145) do
+ActiveRecord::Schema.define(version: 20170316092020) do
 
   create_table "api_tokens", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string  "token"
