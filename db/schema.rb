d26a2879e0f0a7a51e713c909b453f81cf32f9d3
Ruben Schuller <schuller@bytemine.net>
2017-04-28 17:22:05 +0200

    add owner to api_tokens

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170310105145) do
+ActiveRecord::Schema.define(version: 20170428145943) do
 
   create_table "api_tokens", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string  "token"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170310105145) do
     t.boolean "write"
     t.string  "name"
     t.string  "description"
+    t.integer "owner_id"
   end
 
   create_table "attachments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -85,19 +86,19 @@ ActiveRecord::Schema.define(version: 20170310105145) do
     t.index ["nic_id"], name: "index_ip_addresses_on_nic_id", using: :btree
   end
 
-  create_table "location_hierarchies", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "location_hierarchies", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer "ancestor_id",   null: false
     t.integer "descendant_id", null: false
     t.integer "generations",   null: false
   end
 
-  create_table "location_levels", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "location_levels", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string  "name"
     t.string  "description"
     t.integer "level"
   end
 
-  create_table "locations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "locations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string  "name"
     t.string  "description"
     t.integer "level"
