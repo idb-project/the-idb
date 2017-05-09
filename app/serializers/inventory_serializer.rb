0332b367475759d666f5cb9a8af4e7d732d96d6a
Ruben Schuller <schuller@bytemine.net>
2017-05-09 09:15:39 +0200

    fix formatting

--- app/serializers/inventory_serializer.rb
+++ app/serializers/inventory_serializer.rb
@@ -2,10 +2,10 @@ class InventorySerializer < ActiveModel::Serializer
   attributes :id, :inventory_number, :name, :serial, :part_number, :purchase_date, :warranty_end, :seller, :created_at, :updated_at, :user_id, :machine, :deleted_at, :comment, :place, :category, :location_id, :install_date, :inventory_status_id
 
   def machine
-  m = Machine.find_by_id(object.machine_id)
-  unless m
-    return nil
+    m = Machine.find_by_id(object.machine_id)
+    unless m
+      return nil
+    end
+    m.fqdn
   end
-  m.fqdn
-end
 end
