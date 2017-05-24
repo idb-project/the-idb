1ceacbbd98855ace04161130eac20fce27feffcb
Felix Kronlage <kronlage@bytemine.net>
2017-05-25 00:47:28 +0200

    base of the tag, fixes #4968

--- scripts/mark-version.sh
+++ scripts/mark-version.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+set -x
+
 COMMIT=`git rev-parse --short HEAD`
-BRANCH=`git branch | grep \* | cut -d ' ' -f2`
-VERSION=`cat VERSION`
+#BRANCH=`git branch | grep \* | cut -d ' ' -f2`
+TAG=`git tag | tail -1 | cut -d ' ' -f2`
 
 if [[ "$BRANCH" == release* ]]; then
-	VERSIONSTRING="${VERSION}"
+	VERSIONSTRING="${TAG}"
 else
-	VERSIONSTRING="${VERSION}-${BRANCH}-${COMMIT}"
+	VERSIONSTRING="${TAG}-${COMMIT}"
 fi
 
 cat > config/initializers/version.rb <<EOF
