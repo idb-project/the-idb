a555ce36efe0230872a0bf8b868e290bd7e19ac9
Ruben Schuller <schuller@bytemine.net>
2017-07-12 17:58:41 +0200

    fix paths in post install script
    - packager.io packages install to /opt/the-idb instead of /opt/idb
    - change /etc/idb to /etc/the-idb as well to be consistent

--- scripts/debian/postinst.sh
+++ scripts/debian/postinst.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # remove existing symlink to config
-if [ -L /opt/idb/config ]; then
+if [ -L /opt/the-idb/config ]; then
   rm /opt/idb/config
 end
 
 # create symlink to config
-ln -s /etc/idb /opt/idb/config
+ln -s /etc/the-idb /opt/the-idb/config
 
 # copy initializers
-cp /opt/idb/config-example/initializers/{app_config.rb,assets.rb,backtrace_silencers.rb,filter_parameter_logging.rb,inflections.rb,mime_types.rb,paper_trail.rb,raven.rb,redis.rb,rubius.rb,session_store.rb,sidekiq.rb,simple_form.rb,simple_form_bootstrap.rb,version.rb,wrap_parameters.rb} /etc/idb/initializers
+cp /opt/the-idb/config-example/initializers/{app_config.rb,assets.rb,backtrace_silencers.rb,filter_parameter_logging.rb,inflections.rb,mime_types.rb,paper_trail.rb,raven.rb,redis.rb,rubius.rb,session_store.rb,sidekiq.rb,simple_form.rb,simple_form_bootstrap.rb,version.rb,wrap_parameters.rb} /etc/the-idb/initializers
