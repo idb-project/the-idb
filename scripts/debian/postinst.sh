625bd9f9bf71159a0695ae1d8d29c0ffb8bff830
Ruben Schuller <schuller@bytemine.net>
2017-07-14 16:17:52 +0200

    sh doesn't understand braces expansion, just use multiple cp commands

--- scripts/debian/postinst.sh
+++ scripts/debian/postinst.sh
@@ -26,4 +26,19 @@ if [ ! -d "$IDBETC/initializers" ]; then
 fi
 
 # copy selected initializers
-cp $IDBPATH/config-example/initializers/{app_config.rb,assets.rb,backtrace_silencers.rb,filter_parameter_logging.rb,inflections.rb,mime_types.rb,paper_trail.rb,raven.rb,redis.rb,rubius.rb,session_store.rb,sidekiq.rb,simple_form.rb,simple_form_bootstrap.rb,version.rb,wrap_parameters.rb} $IDBETC/initializers
+cp $IDBPATH/config-example/initializers/app_config.rb $IDBETC/initializers
+cp $IDBPATH/config-example/initializers/assets.rb $IDBETC/initializers
+cp $IDBPATH/config-example/initializers/backtrace_silencers.rb $IDBETC/initializers
+cp $IDBPATH/config-example/initializers/filter_parameter_logging.rb $IDBETC/initializers
+cp $IDBPATH/config-example/initializers/inflections.rb $IDBETC/initializers
+cp $IDBPATH/config-example/initializers/mime_types.rb $IDBETC/initializers
+cp $IDBPATH/config-example/initializers/paper_trail.rb $IDBETC/initializers
+cp $IDBPATH/config-example/initializers/raven.rb $IDBETC/initializers
+cp $IDBPATH/config-example/initializers/redis.rb $IDBETC/initializers
+cp $IDBPATH/config-example/initializers/rubius.rb $IDBETC/initializers
+cp $IDBPATH/config-example/initializers/session_store.rb $IDBETC/initializers
+cp $IDBPATH/config-example/initializers/sidekiq.rb $IDBETC/initializers
+cp $IDBPATH/config-example/initializers/simple_form.rb $IDBETC/initializers
+cp $IDBPATH/config-example/initializers/simple_form_bootstrap.rb $IDBETC/initializers
+cp $IDBPATH/config-example/initializers/version.rb $IDBETC/initializers
+cp $IDBPATH/config-example/initializers/wrap_parameters.rb $IDBETC/initializers
