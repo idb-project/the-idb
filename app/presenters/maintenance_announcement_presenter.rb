01e10b857305242eafeb020ff31b4daa39790038
Ruben Schuller <schuller@bytemine.net>
2018-03-12 17:08:16 +0100

    links in announcements table to machines, owners and tickets

---
+++ app/presenters/maintenance_announcement_presenter.rb
@@ -0,0 +1,31 @@
+class MaintenanceAnnouncementPresenter < Keynote::Presenter
+    presents :maintenance_announcement
+
+    delegate :id, :date, :reason, :impact, to: :maintenance_announcement
+
+    def ticket_links
+        links = Array.new
+        maintenance_announcement.maintenance_tickets.pluck(:ticket_id).each do |ticket_id|
+            links << link_to(ticket_id, "http://rt/#{ticket_id}")
+        end
+        links.join(", ").html_safe
+    end
+
+    def owner_links
+        owner_names = Set.new
+        maintenance_announcement.owners.each do |owner|
+            owner_names << link_to(owner.display_name, owner_path(owner))
+        end
+        owner_names.to_a.join(", ").html_safe
+    end
+
+    def machine_links
+        links = ['<ul style="list-style-type: none; margin: 0;">']
+        maintenance_announcement.machines.each do |machine|
+            puts machine.fqdn
+            links << "<li>" + link_to(machine.fqdn, machine_path(machine)) + "</li>"
+        end
+        links << "</ul>"
+        links.join(" ").html_safe
+    end
+end
