746205be8cff44bf74576d1764766edaa4bae788
Ruben Schuller <schuller@bytemine.net>
2018-03-15 18:08:26 +0100

    add formatting of the announcement to maintenance ticket

--- app/models/maintenance_ticket.rb
+++ app/models/maintenance_ticket.rb
@@ -2,4 +2,14 @@ class MaintenanceTicket < ApplicationRecord
   belongs_to :maintenance_announcement
   has_and_belongs_to_many :machines
   has_many :owners, :through => :machines
+
+  def format
+    a = maintenance_announcement
+    t = a.maintenance_template
+    t.format({date: a.date.strftime("%Y-%m-%d"), reason: a.reason, impact: a.impact, machines: format_machines_fqdns })
+  end
+
+  def format_machines_fqdns
+    machines.pluck(:fqdn).join(", ")
+  end
 end
