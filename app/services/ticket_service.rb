1b7c20fa083e006a34477c5b474851e7115ceea3
Ruben Schuller <schuller@bytemine.net>
2018-03-16 15:13:46 +0100

    fix ticket service
    ticket creation now works and the number of the created ticket is
    returned

--- app/services/ticket_service.rb
+++ app/services/ticket_service.rb
@@ -1,25 +1,36 @@
 class TicketService
-    def initialize(ticket)
-        @ticket = ticket
-    end
-
-    def send
-        body = @ticket.format
-        @ticket.ticket_id = 123
-        @ticket.save!
+    def self.send(ticket)
+        text = ticket.format
+        queue = IDB.config.rt.queue
+        requestor = IDB.config.rt.requestor
+        cc = ["schuller@bytemine.net"]
+        ticket_id = TicketService.create_rt_ticket(queue, requestor, cc, "Test Ticket", text)
+        ticket.ticket_id = ticket_id
+        ticket.save!
     end
 
     def self.create_rt_ticket(queue, requestor, cc, subject, text)
         uri = URI(IDB.config.rt.create_ticket_url)
-        uri.query = URI.encode_www_form({user: IDB.config.rt.user, password: IDB.config.rt.password })
-
-        http = Net::HTTP.net(uri.host, uri.port)
+        uri.query = URI.encode_www_form({user: IDB.config.rt.user, pass: IDB.config.rt.password })
+        
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = uri.scheme == 'https'
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
         http.read_timeout = 5
 
         req = Net::HTTP::Post.new(uri.request_uri)
-        req.body = TicketService.encode_rt_ticket(queue, requestor, cc, subject, text)
+        req.body = URI.encode_www_form({content: TicketService.encode_rt_ticket(queue, requestor, cc, subject, text)})
 
+       
         res = http.request(req)
+        
+        m = /# Ticket ([0-9]+) created\./.match(res.body)
+        
+        if res.code != "200" or m.size < 2
+            return nil
+        end
+        
+        return m[1].to_i
     end
 
     def self.encode_rt_ticket(queue, requestor, cc, subject, text)
