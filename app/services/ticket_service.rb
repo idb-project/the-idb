e88c953aa5b8e50da63afcfcd7dfd028b66371f5
Ruben Schuller <schuller@bytemine.net>
2018-08-29 15:04:33 +0200

    send the announcement as reply to machine owner
    to have the announcement sent to the machines owner, first create a
    rt ticket, then add a reply with the machines owner contact address
    as cc.
    the reason for this is that without additional configuration, created
    tickets aren't send to the cc, and using the contact address as
    requestor adds the usual "we've received your message" preamble,
    which we don't want in this case.

--- app/services/ticket_service.rb
+++ app/services/ticket_service.rb
@@ -4,23 +4,29 @@ class TicketService
         subject = ticket.format_subject
         queue = IDB.config.rt.queue
         requestor = IDB.config.rt.requestor
-        cc = [ ticket.owner.announcement_contact ]
-        ticket_id = TicketService.create_rt_ticket(queue, requestor, cc, subject, text)
+
+        # create ticket
+        ticket_id = TicketService.create_rt_ticket(queue, requestor, subject, text)
         ticket.ticket_id = ticket_id
+
+        # comment ticket to send announcement to real contact in cc
+        cc = [ ticket.owner.announcement_contact ]
+        TicketService.reply_rt_ticket(ticket_id, cc, subject, text)
+
         ticket.save!
     end
 
     private
 
-    def self.create_rt_ticket(queue, requestor, cc, subject, text)
-        uri = self.build_uri
+    def self.create_rt_ticket(queue, requestor, subject, text)
+        uri = self.build_create_uri
         
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = uri.scheme == 'https'
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
         http.read_timeout = 5
 
-        req = self.build_request(uri, queue, requestor, cc, subject, text)
+        req = self.build_create_request(uri, queue, requestor, subject, text)
        
         res = http.request(req)
         
@@ -36,38 +42,68 @@ class TicketService
         return ticket_id
     end
 
-    def self.encode_rt_ticket(queue, requestor, cc, subject, text)
+    def self.encode_create_ticket(queue, requestor, subject, text)
         text = self.indent(text)
 
         x = %q(id: new
 Queue: %{queue}
 Requestor: %{requestor}
 Subject: %{subject}
-Cc: %{cc}
 Text: %{text}
 )
-        x % {queue: queue, requestor: requestor, cc: cc.join(","), subject: subject, text: text }
+        x % {queue: queue, requestor: requestor, subject: subject, text: text }
     end
 
-    # RT wants every line except for the first one to be indented with one space
-    def self.indent(text)
-        lines = text.split("\n")
-        lines_new = [lines[0]]
-        lines[1..-1].each do |line|
-            lines_new << " #{line}"
-        end
-        lines_new.join("\n")
-    end
-
-    def self.build_uri
+    def self.build_create_uri
         uri = URI(IDB.config.rt.create_ticket_url)
         uri.query = URI.encode_www_form({user: IDB.config.rt.user, pass: IDB.config.rt.password })
         uri
     end
 
-    def self.build_request(uri, queue, requestor, cc, subject, text)
+    def self.build_create_request(uri, queue, requestor, subject, text)
         req = Net::HTTP::Post.new(uri.request_uri)
-        req.body = URI.encode_www_form({content: TicketService.encode_rt_ticket(queue, requestor, cc, subject, text)})
+        req.body = URI.encode_www_form({content: TicketService.encode_create_ticket(queue, requestor, subject, text)})
+        req
+    end
+
+    def self.reply_rt_ticket(ticket_id, cc, subject, text)
+        uri = self.build_reply_uri(ticket_id)
+        
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = uri.scheme == 'https'
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        http.read_timeout = 5
+
+        req = self.build_reply_request(uri, ticket_id, cc, subject, text)
+
+        res = http.request(req)
+        
+        if res.code != "200" 
+            return nil
+        end
+    end
+
+    def self.encode_reply_ticket(ticket_id, cc, subject, text)
+        text = self.indent(text)
+
+        x = %q(id: %{ticket_id}
+Action: correspond
+Cc: %{cc}
+Subject: %{subject}
+Text: %{text}
+)
+        x % {ticket_id: ticket_id, cc: cc.join(","), subject: subject, text: text }
+    end
+
+    def self.build_reply_uri(ticket_id)
+        uri = URI(IDB.config.rt.reply_ticket_url % ticket_id)
+        uri.query = URI.encode_www_form({user: IDB.config.rt.user, pass: IDB.config.rt.password })
+        uri
+    end
+
+    def self.build_reply_request(uri, ticket_id, cc, subject, text)
+        req = Net::HTTP::Post.new(uri.request_uri)
+        req.body = URI.encode_www_form({content: TicketService.encode_reply_ticket(ticket_id, cc, subject, text)})
         req
     end
 
@@ -79,5 +115,15 @@ Text: %{text}
         end
 
         m[1].to_i
+    end
+
+    # RT wants every line except for the first one to be indented with one space
+    def self.indent(text)
+        lines = text.split("\n")
+        lines_new = [lines[0]]
+        lines[1..-1].each do |line|
+            lines_new << " #{line}"
+        end
+        lines_new.join("\n")
     end
 end
