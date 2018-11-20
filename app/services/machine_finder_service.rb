07285bbd64d727c30c991ed6908418de45b31dff
Daniel Rauer <rauer@bytemine.net>
2018-11-20 12:40:10 +0100

    puppetDB and oxidized configs are optional, #5472.

--- app/services/machine_finder_service.rb
+++ app/services/machine_finder_service.rb
@@ -1,39 +1,49 @@
 class MachineFinderService
   def initialize(redis)
     @list = UntrackedMachinesList.new(redis)
+    @puppet_nodes = []
+    @oxidized_nodes = []
 
-    v3_urls = IDB.config.puppetdb.api_urls.map { |u| [u["url"]] if u["version"] == "v3" }.flatten.compact
-    @nodes = Puppetdb::Nodes.new(v3_urls).all
+    if IDB.config.puppetdb
+      v3_urls = IDB.config.puppetdb.api_urls.map { |u| [u["url"]] if u["version"] == "v3" }.flatten.compact
+      @puppet_nodes = Puppetdb::Nodes.new(v3_urls).all
 
-    v4_urls = IDB.config.puppetdb.api_urls.map { |u| [u["url"]] if u["version"] == "v4" }.flatten.compact
-    @nodes += Puppetdb::NodesV4.new(v4_urls).all
+      v4_urls = IDB.config.puppetdb.api_urls.map { |u| [u["url"]] if u["version"] == "v4" }.flatten.compact
+      @puppet_nodes += Puppetdb::NodesV4.new(v4_urls).all
+    end
 
-    oxidized_urls = IDB.config.oxidized.api_urls.map { |u| [u["url"]] }.flatten.compact
-    @oxidized_nodes = Oxidized::Nodes.new(oxidized_urls).all
+    if IDB.config.oxidized
+      oxidized_urls = IDB.config.oxidized.api_urls.map { |u| [u["url"]] }.flatten.compact
+      @oxidized_nodes = Oxidized::Nodes.new(oxidized_urls).all
+    end
   end
 
   def find_untracked_machines
-    puppet_nodes = @nodes
+    puppet_nodes = @puppet_nodes
     oxidized_nodes = @oxidized_nodes
     machines = Machine.pluck(:fqdn)
     machine_aliases = MachineAlias.pluck(:name)
     puppet_untracked = puppet_nodes - machines - machine_aliases
     oxidized_untracked = oxidized_nodes - machines - machine_aliases
 
-    if IDB.config.puppetdb.auto_create
-      update_untracked(puppet_untracked, "PuppetDB")
-    else
-      # Set all nodes that are not in the database to the untracked machines
-      # list.
-      @list.set(puppet_untracked)
+    if IDB.config.puppetdb
+      if IDB.config.puppetdb.auto_create
+        update_untracked(puppet_untracked, "PuppetDB")
+      else
+        # Set all nodes that are not in the database to the untracked machines
+        # list.
+        @list.set(puppet_untracked)
+      end
     end
 
-    if IDB.config.oxidized.auto_create
-      update_untracked(oxidized_untracked, "Oxidized")
-    else
-      # Add all nodes that are not in the database to the untracked machines
-      # list.
-      @list.add(oxidized_untracked)
+    if IDB.config.oxidized
+      if IDB.config.oxidized.auto_create
+        update_untracked(oxidized_untracked, "Oxidized")
+      else
+        # Add all nodes that are not in the database to the untracked machines
+        # list.
+        @list.add(oxidized_untracked)
+      end
     end
   end
 
--- spec/services/machine_finder_service_spec.rb
+++ spec/services/machine_finder_service_spec.rb
@@ -5,27 +5,33 @@ describe MachineFinderService do
     before do
       allow(User).to receive(:current).and_return(nil) # no current user present
       @machine = Machine.create!(fqdn: 'test.example.com')
-      IDB.config.puppetdb.auto_create = true
-      IDB.config.puppetdb.api_urls = {}
-      IDB.config.oxidized.auto_create = true
-      IDB.config.oxidized.api_urls = {}
-      allow(Puppetdb::Nodes.new).to receive(:all).and_return([])
-      allow(Puppetdb::NodesV4.new).to receive(:all).and_return([])
-      allow(Oxidized::Nodes.new).to receive(:all).and_return([])
+      if IDB.config.puppetdb
+        IDB.config.puppetdb.auto_create = true
+        IDB.config.puppetdb.api_urls = {}
+        allow(Puppetdb::Nodes.new).to receive(:all).and_return([])
+        allow(Puppetdb::NodesV4.new).to receive(:all).and_return([])
+      end
+      if IDB.config.oxidized
+        IDB.config.oxidized.auto_create = true
+        IDB.config.oxidized.api_urls = {}
+        allow(Oxidized::Nodes.new).to receive(:all).and_return([])
+      end
       @mfs = MachineFinderService.new(nil)
     end
 
     it 'creates a machine that has been found in a PuppetDB but not locally' do
       nodes = ["test2.example.com"]
-      @mfs.instance_variable_set(:@nodes, nodes) 
+      @mfs.instance_variable_set(:@puppet_nodes, nodes)
 
       @mfs.find_untracked_machines
-      expect(Machine.last.fqdn).to eq(nodes.last)
+      if IDB.config.puppetdb
+        expect(Machine.last.fqdn).to eq(nodes.last)
+      end
     end
 
     it 'does not create a machine that already exists' do
       nodes = ["test.example.com"]
-      @mfs.instance_variable_set(:@nodes, nodes) 
+      @mfs.instance_variable_set(:@puppet_nodes, nodes)
 
       expect(Machine).not_to receive(:new).with(fqdn: nodes.last)
       @mfs.find_untracked_machines
@@ -35,11 +41,13 @@ describe MachineFinderService do
       @machine.destroy
 
       nodes = ["test.example.com", "test3.example.com"]
-      @mfs.instance_variable_set(:@nodes, nodes) 
+      @mfs.instance_variable_set(:@puppet_nodes, nodes)
 
       @mfs.find_untracked_machines
       expect(Machine.find_by_fqdn(nodes.first)).to be_nil
-      expect(Machine.find_by_fqdn(nodes.last)).not_to be_nil
+      if IDB.config.puppetdb
+        expect(Machine.find_by_fqdn(nodes.last)).not_to be_nil
+      end
     end
   end
 end
