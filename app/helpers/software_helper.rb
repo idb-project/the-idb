dd920057ff44f819283ecbe5bba8067e0a1421e8
Ruben Schuller <schuller@bytemine.net>
2018-11-29 17:36:51 +0100

    rewrite software search, add != matching

--- app/helpers/software_helper.rb
+++ app/helpers/software_helper.rb
@@ -1,41 +1,46 @@
 module SoftwareHelper
   def self.parse_query(query)
-    software = Array.new
-    versions = Hash.new
+    parsed = Hash.new
 
     if query.blank?
-      return software, versions
+      return parsed
     end
 
     query.split.each { |s|
-      n, v = s.split('=')
-      software << { name: n }
-      if v
-        versions[n] = v
+      if s.index("!=")
+        name, version = s.split("!=")
+        parsed[name] = -> (x) { return x != version }
+      elsif s.index("=")
+        name, version = s.split("=")
+        parsed[name] = -> (x) { return x.start_with?(version) }
       else
-        versions[n] = nil
+        parsed[s] = -> (x) { return true }
       end
     }
 
-    return software, versions
+    return parsed
   end
 
-  def self.software_machines(all_machines, software, versions)
-    machines = Array.new
-    ms = all_machines.includes(:owner, nics: [:ip_address]).order(:fqdn).where('JSON_CONTAINS(software, ?)', ActiveSupport::JSON.encode(software))
+  def self.software_machines(all_machines, parsed_query)
+    puts "Query:"
+    puts parsed_query.inspect
 
-    ms.each { |m|
-      ignore = false
+    machines = Array.new
+    
+    # get all machines which have all software installed
+    ms = all_machines.includes(:owner, nics: [:ip_address]).order(:fqdn).where('JSON_CONTAINS(software, ?)', ActiveSupport::JSON.encode(parsed_query.keys.map { |n| {"name" => n}}))
+    puts "All machines"
+    puts ms.all.inspect
+
+
+    ms.each { |m| 
       m.software.each { |s| 
-        next if not versions[s["name"]]
-        if not s["version"].start_with? versions[s["name"]]
-          ignore = true
-          break
-        end
+        next if not parsed_query[s["name"]]
+        next if not parsed_query[s["name"]].call(s["version"])
+        machines << m
       }
-      next if ignore
-      machines << m
     }
+
     return machines
   end
 end
