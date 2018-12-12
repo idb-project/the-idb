30ab8e4d10030c93f5536afeabd3a76563095c3d
Daniel Rauer <rauer@bytemine.net>
2018-12-13 00:11:54 +0100

    Some more options for code blocks to the markdown renderer, #487.

--- app/core/textile_renderer.rb
+++ app/core/textile_renderer.rb
@@ -1,5 +1,5 @@
 class TextileRenderer
   def self.render(string)
-    Redcarpet::Markdown.new(Redcarpet::Render::HTML, tables: true).render(string).html_safe
+    Redcarpet::Markdown.new(Redcarpet::Render::HTML, {tables: true, fenced_code_blocks: true, lax_spacing: true}).render(string).html_safe
   end
 end
