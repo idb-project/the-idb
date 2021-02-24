b7b268c9e238de2d9d08d30033c4af572d812647
Daniel Rauer <rauer@bytemine.net>
2021-02-24 17:50:49 +0100

    More travis CI adaptions.

--- scripts/travis-reset-mysql-root-password.sh
+++ scripts/travis-reset-mysql-root-password.sh
@@ -2,7 +2,7 @@
 set -x
 set -e
 sudo service mysql stop || echo "mysql not stopped"
-sudo stop mysql-5.6 || echo "mysql-5.6 not stopped"
+sudo stop mysql-5.7 || echo "mysql-5.7 not stopped"
 sudo  mysqld_safe --skip-grant-tables &
 sleep 4
 sudo mysql -e "use mysql; update user set authentication_string=PASSWORD('') where User='root'; update user set plugin='mysql_native_password';FLUSH PRIVILEGES;"
