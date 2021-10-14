2e41dd4dd0e3ea9b28cea6ed47f3afdc264baf7c
Daniel Rauer <rauer@bytemine.net>
2021-10-14 15:59:30 +0200

    Fix indention.

--- app/workers/scheduled_maintenance_announcement_gc_worker.rb
+++ app/workers/scheduled_maintenance_announcement_gc_worker.rb
@@ -3,9 +3,9 @@ class ScheduledMaintenanceAnnouncementGCWorker
   
 	def perform
 	  MaintenanceAnnouncement.where("preview = true AND created_at < ?", Time.now - 1.day).each do |announcement|
-		MaintenanceTicket.where(maintenance_announcement: announcement).destroy_all
-		announcement.destroy
+		  MaintenanceTicket.where(maintenance_announcement: announcement).destroy_all
+		    announcement.destroy
+	    end
 	  end
-	end
   end
   
