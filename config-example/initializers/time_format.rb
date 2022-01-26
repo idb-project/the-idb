46daa68a2ddfe400671d07e23558cdc7774d3606
Ruben Schuller <schuller@bytemine.net>
2022-01-26 11:25:26 +0100

    add missing config fix

--- config-example/initializers/time_format.rb
+++ config-example/initializers/time_format.rb
@@ -1,4 +1,4 @@
 Time::DATE_FORMATS[:announcement_date] = '%d.%m.%Y'
 Time::DATE_FORMATS[:announcement_time] = '%H:%M'
 Time::DATE_FORMATS[:announcement_full] = '%d.%m.%Y %H:%M'
-Time::DATE_FORMATS[:announcement_ical] = '%Y.%m.%dT%H%M%S'
+Time::DATE_FORMATS[:announcement_ical] = '%Y%m%dT%H%M%S'
