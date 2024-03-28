506aae1a17a997d1a84afa494f0aaf0977e9ecff
Daniel Rauer <dra@bytemine.net>
2024-03-28 18:04:33 +0100

    Insert package name and version search strings into search fields.

--- app/controllers/softwares_controller.rb
+++ app/controllers/softwares_controller.rb
@@ -1,5 +1,7 @@
 class SoftwaresController < ApplicationController
   def index
     @machines = SoftwareHelper.software_machines(Machine.all, params["package"], params["version"])
+    @package = params["package"]
+    @version = params["version"]
   end
 end
